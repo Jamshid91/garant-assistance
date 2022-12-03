c07acd083db582e748dfb54eb1d104a34f3f84dc
Jamshid91 <iymon0217@gmail.com>
2022-12-03 21:28:00 +0500

    added

--- telegram.php
+++ telegram.php
@@ -1,6 +1,7 @@
 <?php
 $name = $_POST['user_name'];
 $phone = $_POST['user_phone'];
+$email = $_POST['user_email'];
 $token = "5609776173:AAHqbmmmXY9-VDymzT1bKqDIn9t6U0T9Erw";
 $chat_id = "-717310466";
 $arr = array(
@@ -15,7 +16,8 @@ foreach($arr as $key => $value) {
 $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 if ($sendToTelegram) {
-} else {
-  echo "Error";
-}
+    header('Location: than-you.html');
+  } else {
+    echo "Error";
+  }
 ?>
