cd5ffa76d6b80b7fcfe04494e752c19d7f51b410
Jamshid91 <iymon0217@gmail.com>
2022-12-03 21:14:00 +0500

    added

--- telegram.php
+++ telegram.php
@@ -1,12 +1,8 @@
 <?php
-
-/* https://api.telegram.org/bot5609776173:AAHqbmmmXY9-VDymzT1bKqDIn9t6U0T9Erw/getUpdates,
-где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
-
 $name = $_POST['user_name'];
 $phone = $_POST['user_phone'];
 $token = "5609776173:AAHqbmmmXY9-VDymzT1bKqDIn9t6U0T9Erw";
-$chat_id = "-717310466,";
+$chat_id = "-717310466";
 $arr = array(
   'Имя пользователя: ' => $name,
   'Телефон: ' => $phone
